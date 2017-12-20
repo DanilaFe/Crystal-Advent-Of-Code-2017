e92c3f54e33ba3dc945c67b8296a0831e5f64cd0
Danila Fedorin <danila.fedorin@gmail.com>
2017-12-20 15:12:36 -0800

    Add solutions to day 12.

---
+++ day12_2.cr
@@ -0,0 +1,29 @@
+def find_group(paths, current)
+    visited = [] of Int32
+    queue = [current]
+    while !queue.empty?
+        current = queue.first
+        if !visited.includes? current
+            visited << current
+            queue.concat(paths[current])
+        end
+        queue.delete current
+    end
+    return visited
+end
+
+paths = {} of Int32 => Array(Int32)
+ex = /(\d+) <-> (.+)/
+File.read("day12.txt").chomp.split("\n").each do |it|
+    match = ex.match(it).as Regex::MatchData
+    from = match[1].to_i
+    to = match[2].split(", ").map &.to_i
+    paths[from] = to
+end
+all_numbers = paths.keys
+count = 0
+while !all_numbers.empty?
+    all_numbers -= find_group(paths, all_numbers[0])
+    count += 1
+end
+puts count
